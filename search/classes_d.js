574ee2e30e03abecb6288f4406afcfd179c9d00a
rozmansi <rozmansi@users.noreply.github.com>
2022-02-10 11:16:58 +0000

    deploy: 328646b2d9d7100afe9d2d0a25e2c656241bb25b

--- search/classes_d.js
+++ search/classes_d.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['user_5fimpersonator_0',['user_impersonator',['../classwinstd_1_1user__impersonator.html',1,'winstd']]]
+  ['unmapviewoffile_5fdelete_0',['UnmapViewOfFile_delete',['../structwinstd_1_1_unmap_view_of_file__delete.html',1,'winstd']]],
+  ['unmapviewoffile_5fdelete_3c_20_5fty_5b_5d_3e_1',['UnmapViewOfFile_delete&lt; _Ty[]&gt;',['../structwinstd_1_1_unmap_view_of_file__delete_3_01___ty_0f_0e_4.html',1,'winstd']]],
+  ['user_5fimpersonator_2',['user_impersonator',['../classwinstd_1_1user__impersonator.html',1,'winstd']]]
 ];
