013e6b1625701c5f4c26130de92d5d12db7b0f97
Simon Rozman <simon@rozman.si>
2022-03-07 15:02:22 +0100

    UnitTests: Include all headers
    See if CodeQL provides better coverage then.
    
    Signed-off-by: Simon Rozman <simon@rozman.si>

--- UnitTests/pch.h
+++ UnitTests/pch.h
@@ -5,7 +5,22 @@
 
 #pragma once
 
+#define SECURITY_WIN32
+#define _WINSOCKAPI_	// Prevent inclusion of winsock.h in windows.h
+
+#include <WinStd/COM.h>
+#include <WinStd/Cred.h>
+#include <WinStd/Crypt.h>
+#include <WinStd/EAP.h>
+#include <WinStd/ETW.h>
+#include <WinStd/GDI.h>
+#include <WinStd/MSI.h>
+#include <WinStd/Sec.h>
+#include <WinStd/SetupAPI.h>
 #include <WinStd/Shell.h>
 #include <WinStd/Win.h>
+#include <WinStd/WinSock2.h>
+#include <WinStd/WinTrust.h>
+#include <WinStd/WLAN.h>
 
 #include <CppUnitTest.h>
