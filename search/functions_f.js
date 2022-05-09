fe81ade21d08694943e96fbcc3b640f95902a75c
rozmansi <rozmansi@users.noreply.github.com>
2022-05-09 12:17:23 +0000

    deploy: 21407f2b42a9a8398a60bfc435a4a7924b2745c9

--- search/functions_f.js
+++ search/functions_f.js
@@ -1,18 +1,17 @@
 var searchData=
 [
   ['reason_0',['reason',['../classwinstd_1_1eap__runtime__error.html#a3329eb549dce7f57f5a59e3f5a16705c',1,'winstd::eap_runtime_error']]],
-  ['ref_5funique_5fptr_1',['ref_unique_ptr',['../classwinstd_1_1ref__unique__ptr.html#af092ed7ea1346c7a92b20ae2f6de5577',1,'winstd::ref_unique_ptr::ref_unique_ptr(std::unique_ptr&lt; _Ty, _Dx &gt; &amp;owner)'],['../classwinstd_1_1ref__unique__ptr.html#a755e6f4235fa54330304921ea14b76bc',1,'winstd::ref_unique_ptr::ref_unique_ptr(ref_unique_ptr&lt; _Ty, _Dx &gt; &amp;&amp;other)'],['../classwinstd_1_1ref__unique__ptr_3_01___ty_0f_0e_00_01___dx_01_4.html#a884355151c4c7d65f4ac279966793d5d',1,'winstd::ref_unique_ptr&lt; _Ty[], _Dx &gt;::ref_unique_ptr(std::unique_ptr&lt; _Ty[], _Dx &gt; &amp;owner) noexcept'],['../classwinstd_1_1ref__unique__ptr_3_01___ty_0f_0e_00_01___dx_01_4.html#a2e6a20baa25af8b928d80ccc566e11cc',1,'winstd::ref_unique_ptr&lt; _Ty[], _Dx &gt;::ref_unique_ptr(ref_unique_ptr&lt; _Ty[], _Dx &gt; &amp;&amp;other)']]],
-  ['regcreatekeyexa_2',['RegCreateKeyExA',['../group___win_std_win_a_p_i.html#ga1cba8a6da4757b79b5e416be149dc923',1,'Win.h']]],
-  ['regcreatekeyexw_3',['RegCreateKeyExW',['../group___win_std_win_a_p_i.html#ga131fdda112e5cbfd123718153d925932',1,'Win.h']]],
-  ['regloadmuistringa_4',['RegLoadMUIStringA',['../group___win_std_win_a_p_i.html#ga8baffb9a05cbfe1e198c47e0a1e2cf88',1,'Win.h']]],
-  ['regloadmuistringw_5',['RegLoadMUIStringW',['../group___win_std_win_a_p_i.html#ga3f9a3593107d5333f057570a76e04a57',1,'Win.h']]],
-  ['regopenkeyexa_6',['RegOpenKeyExA',['../group___win_std_win_a_p_i.html#ga2974136cb4530867e14434fb05712b92',1,'Win.h']]],
-  ['regopenkeyexw_7',['RegOpenKeyExW',['../group___win_std_win_a_p_i.html#ga2c61d837a3d96ca9dad3a73df03bf8e4',1,'Win.h']]],
-  ['regquerystringvalue_8',['RegQueryStringValue',['../group___win_std_win_a_p_i.html#gac91030c0badd322d3c64663ceab77b7a',1,'RegQueryStringValue(HKEY hReg, LPCSTR pszName, std::basic_string&lt; char, _Traits, _Ax &gt; &amp;sValue) noexcept:&#160;Win.h'],['../group___win_std_win_a_p_i.html#gaef0a2e894cd51e0003498958008ef825',1,'RegQueryStringValue(HKEY hReg, LPCWSTR pszName, std::basic_string&lt; wchar_t, _Traits, _Ax &gt; &amp;sValue) noexcept:&#160;Win.h']]],
-  ['regqueryvalueexa_9',['RegQueryValueExA',['../group___win_std_win_a_p_i.html#gac75dca7a4e87365ca7021edd82509584',1,'Win.h']]],
-  ['regqueryvalueexw_10',['RegQueryValueExW',['../group___win_std_win_a_p_i.html#ga78f02613f20cc234aad4e1b4726db9ea',1,'Win.h']]],
-  ['repair_11',['repair',['../classwinstd_1_1eap__runtime__error.html#a981cb9a1cbf0c6e7e19252d776a2558f',1,'winstd::eap_runtime_error']]],
-  ['repair_5fid_12',['repair_id',['../classwinstd_1_1eap__runtime__error.html#a1e80ead2a4d348ab2c939bfbbaf9330a',1,'winstd::eap_runtime_error']]],
-  ['root_5fcause_13',['root_cause',['../classwinstd_1_1eap__runtime__error.html#a0aa17a51b2c110e874b60924281a3743',1,'winstd::eap_runtime_error']]],
-  ['root_5fcause_5fid_14',['root_cause_id',['../classwinstd_1_1eap__runtime__error.html#ae39b6b32c9505c0be2e199d8692175d1',1,'winstd::eap_runtime_error']]]
+  ['regcreatekeyexa_1',['RegCreateKeyExA',['../group___win_std_win_a_p_i.html#ga1cba8a6da4757b79b5e416be149dc923',1,'Win.h']]],
+  ['regcreatekeyexw_2',['RegCreateKeyExW',['../group___win_std_win_a_p_i.html#ga131fdda112e5cbfd123718153d925932',1,'Win.h']]],
+  ['regloadmuistringa_3',['RegLoadMUIStringA',['../group___win_std_win_a_p_i.html#ga8baffb9a05cbfe1e198c47e0a1e2cf88',1,'Win.h']]],
+  ['regloadmuistringw_4',['RegLoadMUIStringW',['../group___win_std_win_a_p_i.html#ga3f9a3593107d5333f057570a76e04a57',1,'Win.h']]],
+  ['regopenkeyexa_5',['RegOpenKeyExA',['../group___win_std_win_a_p_i.html#ga2974136cb4530867e14434fb05712b92',1,'Win.h']]],
+  ['regopenkeyexw_6',['RegOpenKeyExW',['../group___win_std_win_a_p_i.html#ga2c61d837a3d96ca9dad3a73df03bf8e4',1,'Win.h']]],
+  ['regquerystringvalue_7',['RegQueryStringValue',['../group___win_std_win_a_p_i.html#gac91030c0badd322d3c64663ceab77b7a',1,'RegQueryStringValue(HKEY hReg, LPCSTR pszName, std::basic_string&lt; char, _Traits, _Ax &gt; &amp;sValue) noexcept:&#160;Win.h'],['../group___win_std_win_a_p_i.html#gaef0a2e894cd51e0003498958008ef825',1,'RegQueryStringValue(HKEY hReg, LPCWSTR pszName, std::basic_string&lt; wchar_t, _Traits, _Ax &gt; &amp;sValue) noexcept:&#160;Win.h']]],
+  ['regqueryvalueexa_8',['RegQueryValueExA',['../group___win_std_win_a_p_i.html#gac75dca7a4e87365ca7021edd82509584',1,'Win.h']]],
+  ['regqueryvalueexw_9',['RegQueryValueExW',['../group___win_std_win_a_p_i.html#ga78f02613f20cc234aad4e1b4726db9ea',1,'Win.h']]],
+  ['repair_10',['repair',['../classwinstd_1_1eap__runtime__error.html#a981cb9a1cbf0c6e7e19252d776a2558f',1,'winstd::eap_runtime_error']]],
+  ['repair_5fid_11',['repair_id',['../classwinstd_1_1eap__runtime__error.html#a1e80ead2a4d348ab2c939bfbbaf9330a',1,'winstd::eap_runtime_error']]],
+  ['root_5fcause_12',['root_cause',['../classwinstd_1_1eap__runtime__error.html#a0aa17a51b2c110e874b60924281a3743',1,'winstd::eap_runtime_error']]],
+  ['root_5fcause_5fid_13',['root_cause_id',['../classwinstd_1_1eap__runtime__error.html#ae39b6b32c9505c0be2e199d8692175d1',1,'winstd::eap_runtime_error']]]
 ];
