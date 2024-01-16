2f7b5ec9b37eba55e4082c5d6cecd01a01cc4af1
rozmansi <rozmansi@users.noreply.github.com>
2024-01-16 15:12:08 +0000

    deploy: 5be20dde2352e7bf5c97e53243a11882b325b2c4

--- search/all_9.js
+++ search/all_9.js
@@ -2,9 +2,10 @@ var searchData=
 [
   ['icon_0',['icon',['../classwinstd_1_1icon.html',1,'winstd']]],
   ['identity_1',['identity',['../group___win_std_e_a_p_a_p_i.html#gga50f5584ca708165f43cec42c42243315aff483d1ff591898a9942916050d2ca3f',1,'winstd']]],
-  ['impersonator_2',['impersonator',['../classwinstd_1_1impersonator.html',1,'winstd::impersonator'],['../classwinstd_1_1impersonator.html#a6d6a8f6446fd2f5bc2120318164f0eac',1,'winstd::impersonator::impersonator()']]],
-  ['initialize_3',['initialize',['../classwinstd_1_1sec__context.html#a7cc49346bd63d78928e65b11b21b6e21',1,'winstd::sec_context']]],
-  ['installer_20api_4',['Microsoft Installer API',['../group___win_std_m_s_i_a_p_i.html',1,'']]],
-  ['invalid_5',['invalid',['../group___win_std_sys_handles.html#gacf43e306968474166474090690857e1c',1,'winstd::handle']]],
-  ['is_20not_6',['What WinStd Is Not',['../index.html#autotoc_md9',1,'']]]
+  ['idispatchinvoke_2',['IDispatchInvoke',['../group___win_std_c_o_m_helpers.html#ga191e4b679c9beb7a04e048f8c00d4884',1,'winstd']]],
+  ['impersonator_3',['impersonator',['../classwinstd_1_1impersonator.html',1,'winstd::impersonator'],['../classwinstd_1_1impersonator.html#a6d6a8f6446fd2f5bc2120318164f0eac',1,'winstd::impersonator::impersonator()']]],
+  ['initialize_4',['initialize',['../classwinstd_1_1sec__context.html#a7cc49346bd63d78928e65b11b21b6e21',1,'winstd::sec_context']]],
+  ['installer_20api_5',['Microsoft Installer API',['../group___win_std_m_s_i_a_p_i.html',1,'']]],
+  ['invalid_6',['invalid',['../group___win_std_sys_handles.html#gacf43e306968474166474090690857e1c',1,'winstd::handle']]],
+  ['is_20not_7',['What WinStd Is Not',['../index.html#autotoc_md9',1,'']]]
 ];
--- search/functions_8.js
+++ search/functions_8.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['impersonator_0',['impersonator',['../classwinstd_1_1impersonator.html#a6d6a8f6446fd2f5bc2120318164f0eac',1,'winstd::impersonator']]],
-  ['initialize_1',['initialize',['../classwinstd_1_1sec__context.html#a7cc49346bd63d78928e65b11b21b6e21',1,'winstd::sec_context']]]
+  ['idispatchinvoke_0',['IDispatchInvoke',['../group___win_std_c_o_m_helpers.html#ga191e4b679c9beb7a04e048f8c00d4884',1,'winstd']]],
+  ['impersonator_1',['impersonator',['../classwinstd_1_1impersonator.html#a6d6a8f6446fd2f5bc2120318164f0eac',1,'winstd::impersonator']]],
+  ['initialize_2',['initialize',['../classwinstd_1_1sec__context.html#a7cc49346bd63d78928e65b11b21b6e21',1,'winstd::sec_context']]]
 ];
