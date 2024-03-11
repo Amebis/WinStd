9da457ab2bdb473eaeb9f94c1d3947e2413f6265
Simon Rozman <simon@rozman.si>
2024-03-11 21:41:51 +0100

    WinHTTP: Add WinHttpQueryHeaders
    Signed-off-by: Simon Rozman <simon@rozman.si>

--- include/WinStd/WinHTTP.h
+++ include/WinStd/WinHTTP.h
@@ -10,6 +10,53 @@
 
 #include "Common.h"
 #include <winhttp.h>
+#include <string>
+
+/// \addtogroup WinStdWinHTTP
+/// @{
+
+///
+/// Retrieves header information associated with an HTTP request.
+///
+/// \sa [WinHttpQueryHeaders function](https://learn.microsoft.com/en-us/windows/win32/api/winhttp/nf-winhttp-winhttpqueryheaders)
+///
+inline _Success_(return) BOOL WinHttpQueryHeaders(_In_ HINTERNET hRequest, _In_ DWORD dwInfoLevel, _Out_ DWORD& dwData)
+{
+    DWORD dwSize = sizeof(dwData);
+    if (WinHttpQueryHeaders(hRequest, dwInfoLevel | WINHTTP_QUERY_FLAG_NUMBER, WINHTTP_HEADER_NAME_BY_INDEX, &dwData, &dwSize, WINHTTP_NO_HEADER_INDEX)) {
+        assert(dwSize == sizeof(dwData));
+        return TRUE;
+    }
+    return FALSE;
+}
+
+///
+/// Retrieves header information associated with an HTTP request.
+///
+/// \sa [WinHttpQueryHeaders function](https://learn.microsoft.com/en-us/windows/win32/api/winhttp/nf-winhttp-winhttpqueryheaders)
+///
+inline _Success_(return) BOOL WinHttpQueryHeaders(_In_ HINTERNET hRequest, _In_ DWORD dwInfoLevel, _Inout_ std::wstring& sData)
+{
+    DWORD dwSize = 0x100;
+    for (;;) {
+        sData.resize(dwSize - 1);
+        dwSize *= sizeof(WCHAR);
+        if (WinHttpQueryHeaders(hRequest, dwInfoLevel, WINHTTP_HEADER_NAME_BY_INDEX, sData.data(), &dwSize, WINHTTP_NO_HEADER_INDEX)) {
+            dwSize /= sizeof(WCHAR);
+            sData.resize(dwSize);
+            return TRUE;
+        }
+        DWORD result = GetLastError();
+        if (result == ERROR_NOT_ENOUGH_MEMORY) {
+            dwSize /= sizeof(WCHAR);
+            dwSize *= 2;
+        }
+        else
+            return FALSE;
+    }
+}
+
+/// @}
 
 namespace winstd
 {
