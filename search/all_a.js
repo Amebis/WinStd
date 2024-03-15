ca69b431b05ce1f9c5fd331ee83104f9c0e7244f
rozmansi <rozmansi@users.noreply.github.com>
2024-03-15 17:46:05 +0000

    deploy: 0559794633d9c2a477e35a6dd0ad1bf5d9dea970

--- search/all_a.js
+++ search/all_a.js
@@ -1,14 +1,15 @@
 var searchData=
 [
-  ['legacy_5fmschapv2_0',['legacy_mschapv2',['../group___win_std_e_a_p_a_p_i.html#gga50f5584ca708165f43cec42c42243315adfe86ce1b68c3cdefe90e35904fc744a',1,'winstd']]],
-  ['legacy_5fpap_1',['legacy_pap',['../group___win_std_e_a_p_a_p_i.html#gga50f5584ca708165f43cec42c42243315a262032c7bb2ef8f08d5b85ee63f79eff',1,'winstd']]],
-  ['length_2',['length',['../classwinstd_1_1bstr.html#aa6970921c6334a993f5f0fc1be5d54e3',1,'winstd::bstr']]],
-  ['library_3',['library',['../classwinstd_1_1library.html',1,'winstd']]],
-  ['load_5fmsg_5ffrom_5fres_4',['load_msg_from_res',['../group___win_std_exceptions.html#ga707a2297f021e381a6ed3751c0aaff92',1,'winstd']]],
-  ['loadstringa_5',['LoadStringA',['../group___win_std_win_a_p_i.html#ga141a51b128dac2b7b0b0f5fddc91fdaf',1,'Win.h']]],
-  ['loadstringw_6',['LoadStringW',['../group___win_std_win_a_p_i.html#ga6c4d84d20f78aac00fe314a7d35d8b48',1,'Win.h']]],
-  ['localfree_5fdelete_7',['LocalFree_delete',['../structwinstd_1_1_local_free__delete.html',1,'winstd::LocalFree_delete&lt; _Ty &gt;'],['../structwinstd_1_1_local_free__delete.html#ae7e35dd11650c49de0ebcab4388c9400',1,'winstd::LocalFree_delete::LocalFree_delete()'],['../structwinstd_1_1_local_free__delete.html#abbb52355375f34eca425d61a59261461',1,'winstd::LocalFree_delete::LocalFree_delete(const LocalFree_delete&lt; _Ty2 &gt; &amp;)'],['../structwinstd_1_1_local_free__delete_3_01___ty_0f_0e_4.html#a34a948cc7b0f12c0f1e4b7e234d8181c',1,'winstd::LocalFree_delete&lt; _Ty[]&gt;::LocalFree_delete()']]],
-  ['localfree_5fdelete_3c_20_5fty_5b_5d_3e_8',['LocalFree_delete&lt; _Ty[]&gt;',['../structwinstd_1_1_local_free__delete_3_01___ty_0f_0e_4.html',1,'winstd']]],
-  ['lookupaccountsida_9',['LookupAccountSidA',['../group___win_std_win_a_p_i.html#ga494161e98275f571eff0da1d34e80145',1,'Win.h']]],
-  ['lookupaccountsidw_10',['LookupAccountSidW',['../group___win_std_win_a_p_i.html#ga55cf815e26d149f0032f1a1c5160fac4',1,'Win.h']]]
+  ['last_5ferror_5fsaver_0',['last_error_saver',['../classwinstd_1_1last__error__saver.html',1,'winstd::last_error_saver'],['../classwinstd_1_1last__error__saver.html#a5ba6e2ccbfc146979babdc962e840882',1,'winstd::last_error_saver::last_error_saver()']]],
+  ['legacy_5fmschapv2_1',['legacy_mschapv2',['../group___win_std_e_a_p_a_p_i.html#gga50f5584ca708165f43cec42c42243315adfe86ce1b68c3cdefe90e35904fc744a',1,'winstd']]],
+  ['legacy_5fpap_2',['legacy_pap',['../group___win_std_e_a_p_a_p_i.html#gga50f5584ca708165f43cec42c42243315a262032c7bb2ef8f08d5b85ee63f79eff',1,'winstd']]],
+  ['length_3',['length',['../classwinstd_1_1bstr.html#aa6970921c6334a993f5f0fc1be5d54e3',1,'winstd::bstr']]],
+  ['library_4',['library',['../classwinstd_1_1library.html',1,'winstd']]],
+  ['load_5fmsg_5ffrom_5fres_5',['load_msg_from_res',['../group___win_std_exceptions.html#ga707a2297f021e381a6ed3751c0aaff92',1,'winstd']]],
+  ['loadstringa_6',['LoadStringA',['../group___win_std_win_a_p_i.html#ga141a51b128dac2b7b0b0f5fddc91fdaf',1,'Win.h']]],
+  ['loadstringw_7',['LoadStringW',['../group___win_std_win_a_p_i.html#ga6c4d84d20f78aac00fe314a7d35d8b48',1,'Win.h']]],
+  ['localfree_5fdelete_8',['LocalFree_delete',['../structwinstd_1_1_local_free__delete.html',1,'winstd::LocalFree_delete&lt; _Ty &gt;'],['../structwinstd_1_1_local_free__delete.html#ae7e35dd11650c49de0ebcab4388c9400',1,'winstd::LocalFree_delete::LocalFree_delete()'],['../structwinstd_1_1_local_free__delete.html#abbb52355375f34eca425d61a59261461',1,'winstd::LocalFree_delete::LocalFree_delete(const LocalFree_delete&lt; _Ty2 &gt; &amp;)'],['../structwinstd_1_1_local_free__delete_3_01___ty_0f_0e_4.html#a34a948cc7b0f12c0f1e4b7e234d8181c',1,'winstd::LocalFree_delete&lt; _Ty[]&gt;::LocalFree_delete()']]],
+  ['localfree_5fdelete_3c_20_5fty_5b_5d_3e_9',['LocalFree_delete&lt; _Ty[]&gt;',['../structwinstd_1_1_local_free__delete_3_01___ty_0f_0e_4.html',1,'winstd']]],
+  ['lookupaccountsida_10',['LookupAccountSidA',['../group___win_std_win_a_p_i.html#ga494161e98275f571eff0da1d34e80145',1,'Win.h']]],
+  ['lookupaccountsidw_11',['LookupAccountSidW',['../group___win_std_win_a_p_i.html#ga55cf815e26d149f0032f1a1c5160fac4',1,'Win.h']]]
 ];
--- search/classes_9.js
+++ search/classes_9.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['library_0',['library',['../classwinstd_1_1library.html',1,'winstd']]],
-  ['localfree_5fdelete_1',['LocalFree_delete',['../structwinstd_1_1_local_free__delete.html',1,'winstd']]],
-  ['localfree_5fdelete_3c_20_5fty_5b_5d_3e_2',['LocalFree_delete&lt; _Ty[]&gt;',['../structwinstd_1_1_local_free__delete_3_01___ty_0f_0e_4.html',1,'winstd']]]
+  ['last_5ferror_5fsaver_0',['last_error_saver',['../classwinstd_1_1last__error__saver.html',1,'winstd']]],
+  ['library_1',['library',['../classwinstd_1_1library.html',1,'winstd']]],
+  ['localfree_5fdelete_2',['LocalFree_delete',['../structwinstd_1_1_local_free__delete.html',1,'winstd']]],
+  ['localfree_5fdelete_3c_20_5fty_5b_5d_3e_3',['LocalFree_delete&lt; _Ty[]&gt;',['../structwinstd_1_1_local_free__delete_3_01___ty_0f_0e_4.html',1,'winstd']]]
 ];
